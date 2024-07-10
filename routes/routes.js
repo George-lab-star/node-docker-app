e44d986e7b56e7a33cd68c7520e19527fc968e90
George-lab-star <georgelomakin99@gmail.com>
2024-07-10 06:16:25 -0500

    add routes controller

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
-const controllerGetCurrencyList = require('./controllers/controllerGetCurrencyList.js');
-const controllerConvertCurrency = require('./controllers/controllerConvertCurrency.js');
+const controllerGetCurrencyList = require('../controllers/controllerGetCurrencyList.js');
+const controllerConvertCurrency = require('../controllers/controllerConvertCurrency.js');
 const express = require('express');
 const router = express.Router();
 
+router.use(express.json())
+
 router.get("/currency/list", controllerGetCurrencyList.getCurrencyList);
 
-router.get("/currency/convert", controllerConvertCurrency.convertCurrency);
+router.post("/currency/convert", controllerConvertCurrency.convertCurrency);
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const routes = require('./routes/routes.js');
+const routes = require('../routes/routes.js');
 
 const app = express();
 
 const port = 3000;
-
-app.use(bodyParser.json());
 
 app.use('/', routes);
 
